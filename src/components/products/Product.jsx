e248358a078788c35c8e956ae00300f6c785decb
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2025-01-14 11:18:31 +0530

    14-01-2024

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getProducts } from '../../services/ProductService'
+import ProductItem from './ProductItem'
+import ProductForm from './ProductForm'
 
 function Product() {
+  let [products, setProducts] = useState([])
+
+  useEffect(() => {
+
+    getProducts().then(data => {
+      setProducts(data);
+    })
+
+  }, [])
+
   return (
-    <div>Product</div>
+    <div>
+      <div class="container mt-3">
+        <div class="row">
+
+          <div class="col">
+            {/*ProductForm Start */}
+            <ProductForm />
+            {/*ProductForm End */}
+          </div>
+
+
+          <div class="col">
+            {/* Displaying Products :Start */}
+            <div class="row row-cols-1 row-cols-md-2 g-4">
+
+              {products.map((p) => {
+                return (
+                  <ProductItem
+                    productName={p.productName}
+                    productDescription={p.productDescription}
+                    productPrice={p.productprice}
+                  />
+                )
+              })}
+            </div>
+            {/* Displaying Products :End */}
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ src/services/ProductService.jsx
@@ -0,0 +1,10 @@
+//Logic
+
+const API_LINK="http://localhost:8080/products"
+
+export const getProducts=()=>{
+    return fetch(API_LINK)
+            .then(data=>data.json())
+            .then(data=>data["_embedded"]["products"])
+}
+
