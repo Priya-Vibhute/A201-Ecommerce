4d6ea3e47c89d00de7ab9d07ed40f74ae827fa63
Priya-Vibhute <priyanka.vibhute@itvedant.com>
2025-01-15 09:16:11 +0530

    15-01-2025

--- src/components/products/ProductForm.jsx
+++ src/components/products/ProductForm.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import { addProduct } from '../../services/ProductService'
 
 function ProductForm() {
+
+    // Function to be called when form will be submitted
+     
+    const submitHandler=(e)=>{
+
+       addProduct({
+          productId:e.target.productId.value,
+          productName:e.target.productName.value,
+          productDescription:e.target.productDescription.value,
+          productprice:e.target.productprice.value
+       }).then(data=>data)
+
+    }
+
+    // ================================================
+
     return (
         <div className='container border border-primary border-3 p-3 my-3'>
-            <form>
+            <form onSubmit={submitHandler}>
                 <h1 className='bg-primary p-2 text-white text-center'>Add Product</h1>
                 {/* Product Id */}
                 <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Product Id</label>
-                    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='productId' />
+                    <input type="number" className="form-control" id="exampleInputEmail1" 
+                    aria-describedby="emailHelp" name='productId' />
                 </div>
 
                 {/* Product Name */}
@@ -26,7 +44,7 @@ function ProductForm() {
                   {/* Product Price */}
                   <div className="mb-3">
                     <label for="exampleInputEmail1" className="form-label">Product price</label>
-                    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='productPrice' />
+                    <input type="number" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='productprice' />
                 </div>
 
                   {/* Button to submit form  */}
